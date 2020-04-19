a7ac81fa63815c49dc395d0e750369129a80824f
Connor Adams <connorads@users.noreply.github.com>
2020-04-19 19:15:22 +0100

    Generalise solution

--- src/Speller.hs
+++ src/Speller.hs
@@ -3,8 +3,11 @@ module Speller (speller) where
 getPhrase :: String -> String
 getPhrase word = ([head word]) ++ " is for " ++ word
 
+combinePhrase :: String -> [String] -> String
+combinePhrase phrase (word:[]) = phrase ++ ", and " ++ getPhrase word
+combinePhrase phrase (word:words) = combinePhrase (phrase ++ ", " ++ getPhrase word) words
+
 speller :: [String] -> String
 speller [] = ""
-speller (x:[]) = getPhrase x
-speller (x:y:[]) = getPhrase x ++ ", and " ++ getPhrase y
-speller (x:y:z:[]) = getPhrase x ++ ", " ++ getPhrase y ++ ", and " ++ getPhrase z
+speller (word:[]) = getPhrase word
+speller (word:words) = combinePhrase (getPhrase word) words
--- test/SpellerSpec.hs
+++ test/SpellerSpec.hs
@@ -8,13 +8,21 @@ spec = do
   describe "speller" $ do
     it "[] should be \"\"" $ do
       speller [] `shouldBe` ""
+      
     it "[\"abacus\"] should be \"a is for abacus\"" $ do
       speller ["abacus"] `shouldBe` "a is for abacus"
+
     it "[\"banana\"] should be \"b is for banana\"" $ do
       speller ["banana"] `shouldBe` "b is for banana"
+
     it "[\"coconut\"] should be \"c is for coconut\"" $ do
       speller ["coconut"] `shouldBe` "c is for coconut"
+
     it "[\"whisky\", \"x-ray\"] should be \"w is for whisky, and x is for x-ray\"" $ do
       speller ["whisky", "x-ray"] `shouldBe` "w is for whisky, and x is for x-ray"
+
     it "[\"apple\", \"banana\", \"coconut\"] should be \"a is for apple, b is for banana, and c is for coconut\"" $ do
       speller ["apple", "banana", "coconut"] `shouldBe` "a is for apple, b is for banana, and c is for coconut"
+
+    it "[\"apple\", \"banana\", \"coconut\", \"doughnut\"] should be \"a is for apple, b is for banana, c is for coconut, and d is for doughnut\"" $ do
+      speller ["apple", "banana", "coconut", "doughnut"] `shouldBe` "a is for apple, b is for banana, c is for coconut, and d is for doughnut"
